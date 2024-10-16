8943afb9ee367f1ddb6ff7a147d147d1a172c067
GracielaSanjurjoFerrin <128257938+GracielaSanjurjoFerrin@users.noreply.github.com>
2024-10-16 15:37:25 +0200

    unify inclination criteria

--- dist/xraybinaryorbit-0.2.8 2/test/test_import.py
+++
@@ -1,7 +0,0 @@
-# test_import.py
-
-def test_import_package():
-    try:
-        import xraybinaryorbit  # Replace with the actual name of your package
-    except ImportError as e:
-        assert False, f"Failed to import the package: {e}"
