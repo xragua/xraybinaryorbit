ad6b1eda958234b81d6ee719dbf64c646af16f67
graciela <graciela@192-168-100-135.lan>
2026-07-10 22:53:12 +0200

    Refactor orbital models and update tests, examples and documentation

---
+++ my-project/docs/javascripts/mathjax.js
@@ -0,0 +1,16 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["$", "$"], ["\\(", "\\)"]],
+    displayMath: [["$$", "$$"], ["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: "tex2jax_ignore",
+    processHtmlClass: "tex2jax_process"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
